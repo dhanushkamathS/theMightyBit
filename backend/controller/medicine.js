fcc28c2ecfa511a82a1964d6287287b656836b52
Udit Pathak <pathakprintudit@gmail.com>
2020-11-18 23:05:09 +0530

    initial start to medicine

--- backend/controller/medicine.js
+++ backend/controller/medicine.js
@@ -1,1 +1,30 @@
-console.log("working");
+var Datastore = require('nedb');
+db = new Datastore({ filename: '../models/datafile', autoload: true });
+
+
+exports.addMedicine = (req,res) => {
+    medicine = req.body
+    db.insert(medicine,(err) => {
+        if(err){
+            return res.status(400).json({
+                error : `error ${err} occured`
+            })
+        }
+    })
+};
+
+exports.deleteMedicine = (req,res) => {
+    medicinename = req.body.medicinename
+    username = req.body.username
+    db.update({username : `${username}`},{$pull : {medicines : {medicinename : `${medicinename}`}}},{},(err) => {
+        if(err){
+            return res.status(400).json({
+                error : `error ${err} occured`
+            })
+        }
+    })
+}
+
+exports.updateMedicine = (req,res) => {
+
+}
