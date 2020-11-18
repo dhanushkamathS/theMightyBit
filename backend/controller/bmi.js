1ac2baa3ca867637fb52f426768eebd365930ad7
Udit Pathak <pathakprintudit@gmail.com>
2020-11-18 22:33:48 +0530

    bmi router and controller

--- backend/controller/bmi.js
+++ backend/controller/bmi.js
@@ -1,1 +1,14 @@
-console.log("working");
+var Datastore = require("nedb")
+db = new Datastore({ filename: '../models/datafile', autoload: true });
+
+
+exports.postBMI = (res,req) => {
+    var username = req.username
+    db.update({username : `${username}`},{$set : { age : `${req.age}`, weight : `${req.weight}`,height : `${req.height}`}},{},(err,updatedDocs) => {
+        if (err){
+            res.status(400).json({
+                err : `error occured ${err}`
+            })
+        }
+    });
+};
