a774d1d808c58a0eeb6cac8b6fd21225d94fcf6b
dhanush <dhanushkamath@gmail.com>
2020-11-18 21:44:54 +0530

    signup isSignedin , login routh and controller

--- backend/routers/userAuth.js
+++ backend/routers/userAuth.js
@@ -1,1 +1,11 @@
-console.log("working");
+const express = require("express");
+var userAuth = express.Router();
+const {login,signup , isSignedUp} = require("../controller/userAuth");
+
+//signin route
+userAuth.post("/login",login);
+
+//signup route 
+userAuth.post("/signup",isSignedUp, signup);
+
+module.exports = userAuth;
