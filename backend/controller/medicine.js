273d08cfdf194a1abfd68b262e31448efa0e8e94
Udit Pathak <pathakprintudit@gmail.com>
2020-11-19 07:38:22 +0530

    add and delete of medicine

--- backend/controller/medicine.js
+++ backend/controller/medicine.js
@@ -3,8 +3,9 @@ db = new Datastore({ filename: '../models/datafile', autoload: true });
 
 
 exports.addMedicine = (req,res) => {
-    medicine = req.body
-    db.insert(medicine,(err) => {
+    medicine = req.body.medicine
+    username = req.body.username
+    db.update({username : `${username}`},{$addToSet : {medicines : `${medicine}`}},{},(err)=>{
         if(err){
             return res.status(400).json({
                 error : `error ${err} occured`
@@ -14,7 +15,7 @@ exports.addMedicine = (req,res) => {
 };
 
 exports.deleteMedicine = (req,res) => {
-    medicinename = req.body.medicinename
+    medicinename = req.body.medicine.medicinename
     username = req.body.username
     db.update({username : `${username}`},{$pull : {medicines : {medicinename : `${medicinename}`}}},{},(err) => {
         if(err){
@@ -26,5 +27,14 @@ exports.deleteMedicine = (req,res) => {
 }
 
 exports.updateMedicine = (req,res) => {
-
+    medicinename = req.body.medicine.medicinename
+    username = req.body.username
+    medicine = req.body.medicine
+    db.update({username : `${username}`},{$set : {medicines : `${medicine}`}},{},(err)=>{
+        if(err){
+            return res.status(400).json({
+                error : `error ${err} occured`
+            })
+        }
+    })
 }
